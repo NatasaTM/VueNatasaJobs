8035cf462633d96943eff63c4bcee818431ec17f
Natasa <natasa.t.markovic@gmail.com>
2024-01-29 21:44:52 +0100

    db.json file, fetching data

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,17 +1,34 @@
 <template>
-    <h1>Job Details Page</h1>
+    <div v-if="job">
+        <h1>{{ job.title}}</h1>
     <p>The job ID is {{ id }}</p>
+    <p> {{ job.details }}</p>
+    </div>
+    <div v-else>
+        <p>Loading job details...</p>
+    </div>
+    
 </template>
 
 <script>
     export default {
         props: ['id'],
-     //   data(){
-      //      return {
-        //        id: this.$route.params.id
-       //     }
-      //  }
+        data(){
+          return {
+            job: null
+          }
+        },
+        mounted(){
+            console.log('ID:', this.id); // Log the ID prop
+        fetch('http://localhost:3000/jobs/' + this.id)
+            .then((res) => res.json())
+            .then(data => {
+                console.log('Data:', data); // Log the data returned by the fetch request
+                this.job = data;
+            })
+            .catch(err => console.log(err.message));
     }
+}
 </script>
 
 <style>
